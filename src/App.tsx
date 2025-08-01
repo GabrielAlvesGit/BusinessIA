6e84af97228d167f029b0889119a498af9950440
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-08-01 12:45:37 -0300

    ajustes na arquitetura

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles/scss/App.scss';
-import Header from './components/header/Header';
+import Header from './components/global/Header';
+import Home from './page-template/Home/Home';
 
 const App: React.FC = () => {
     return (
         <>
             <Header />
+            <Home />
         </>
     );
 };
---
+++ src/components/home/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner: React.FC = () => {
+   return (
+        <>
+       <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi voluptates maxime et ex voluptatem voluptate ipsam, illo officiis magnam fugiat sequi itaque libero? Temporibus, laudantium. Distinctio odit unde consequatur officia!</div>
+        </>
+    );
+}
+
+export default Banner;
