1b4989657a71ca2419481b14b6e7a7a38e0b46e5
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-08-01 21:07:39 -0300

    create menu e banner

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -9,46 +9,60 @@ const Header: React.FC = () => {
                 <div className="nav__menu">
                     <ul className="nav__menu__list">
                         <li className="nav__menu__list__item">
-                            <a className="nav__menu__list__item__link" href="#">Os 3 Porquês
-                                <i className="uil uil- nav__icon"></i>
+                            <a className="nav__menu__list__item__link" href="#">
+                                <span>Os 3 Porquês</span>
                             </a>
 
-                            <ul className='submenu'>
-                                <ul className="submenu__list">
-                                    <span className="submenu__list__item__title">Os 3 Porquês</span>
-                                    
-                                    <li className="submenu__list__item">
-                                        <a className="submenu__list__item__link" href="#">
-                                            Por que
-                                            <span className="submenu__list__item__title">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem quos, delectus soluta praesentium nulla itaque atque vel cupiditate quasi odio molestias iusto numquam dignissimos dolores omnis laboriosam voluptatem mollitia architecto.</span>
-                                        </a>
-                                    </li>
+                            <article className="submenu">
+                                <div className="container">
+                                    <ul className="submenu__list">
+                                        <div className="submenu__list__left">
+                                            <span className="submenu__list__left__title">Os 3 Porquês</span>
+                                        </div>
+                                        
+                                        <ul className="submenu__list__right">
+                                            <li className="submenu__list__right__item">
+                                                <a className="submenu__list__right__item__link" href="#">
+                                                    Por que
+                                                </a>
+                                                <p className="submenu__list__right__item__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem quos, delectus soluta praesentium nulla itaque atque vel cupiditate quasi odio molestias iusto numquam dignissimos dolores omnis laboriosam voluptatem mollitia architecto.</p>
+                                            </li>
 
-                                    <li className="submenu__list__item">
-                                        <a className="submenu__list__item__link" href="#">
-                                            Por que
-                                            <span className="submenu__list__item__title">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem quos, delectus soluta praesentium nulla itaque atque vel cupiditate quasi odio molestias iusto numquam dignissimos dolores omnis laboriosam voluptatem mollitia architecto.</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </ul>
+                                            <li className="submenu__list__right__item">
+                                                <a className="submenu__list__right__item__link" href="#">
+                                                    Por que
+                                                </a>
+                                                <p className="submenu__list__right__item__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem quos, delectus soluta praesentium nulla itaque atque vel cupiditate quasi odio molestias iusto numquam dignissimos dolores omnis laboriosam voluptatem mollitia architecto.</p>
+                                            </li>
+
+                                            <li className="submenu__list__right__item">
+                                                <a className="submenu__list__right__item__link" href="#">
+                                                    Por que
+                                                </a>
+                                                <p className="submenu__list__right__item__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem quos, delectus soluta praesentium nulla itaque atque vel cupiditate quasi odio molestias iusto numquam dignissimos dolores omnis laboriosam voluptatem mollitia architecto.</p>
+                                            </li>
+                                           
+                                        </ul>
+                                    </ul>   
+                                </div>
+                            </article>
                         </li>
 
                         <li className="nav__menu__list__item">
                             <a className="nav__menu__list__item__link" href="#">Plataformas Digitais
-                                <i className="uil uil- nav__icon"></i>
+                                
                             </a>
                         </li>
                           
                         <li className="nav__menu__list__item">
                             <a className="nav__menu__list__item__link" href="#">Contato
-                                <i className="uil uil- nav__icon"></i>
+                                
                             </a>
                         </li>
 
                         <li className="nav__menu__list__item">
                             <a className="nav__menu__list__item__link" href="#">Quem Somos
-                                <i className="uil uil- nav__icon"></i>
+                                
                             </a>
                         </li>
                     </ul>
--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -2,9 +2,55 @@ import React from 'react';
 
 const Banner: React.FC = () => {
    return (
-        <>
-       <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi voluptates maxime et ex voluptatem voluptate ipsam, illo officiis magnam fugiat sequi itaque libero? Temporibus, laudantium. Distinctio odit unde consequatur officia!</div>
-        </>
+      <section className="banner">
+        <div className="banner__content">
+            <div className="container">
+                <div className='banner__content__top'>
+                    <h1 className="banner__content__top__title">Business Manager</h1>
+                </div>
+
+                <div className="banner__content__bottom">
+                    <ul className="banner__content__bottom__list">
+                        <li className="banner__content__bottom__list__item">
+                            <a className="banner__content__bottom__list__item__link button" href="#">Demo</a>
+                        </li>
+
+                        <li className="banner__content__bottom__list__item">
+                            <a className="banner__content__bottom__list__item__link button" href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-zap w-6 h-6 mr-3" data-lov-id="src/pages/Index.tsx:134:16" data-lov-name="Zap" data-component-path="src/pages/Index.tsx" data-component-line="134" data-component-file="Index.tsx" data-component-name="Zap" data-component-content="%7B%22className%22%3A%22w-6%20h-6%20mr-3%22%7D"><path d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z"></path></svg>
+                                Variables
+                            </a>
+                        </li>
+
+                        <li className="banner__content__bottom__list__item">
+                            <a className="banner__content__bottom__list__item__link button" href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-zap w-6 h-6 mr-3" data-lov-id="src/pages/Index.tsx:134:16" data-lov-name="Zap" data-component-path="src/pages/Index.tsx" data-component-line="134" data-component-file="Index.tsx" data-component-name="Zap" data-component-content="%7B%22className%22%3A%22w-6%20h-6%20mr-3%22%7D"><path d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z"></path></svg>
+                                Icon set
+                            </a>
+                        </li>
+
+                        <li className="banner__content__bottom__list__item">
+                            <a className="banner__content__bottom__list__item__link button" href="#">
+                                Accessibility True
+                            </a>
+                        </li>
+
+                        <li className="banner__content__bottom__list__item">
+                            <a className="banner__content__bottom__list__item__link button" href="#">
+                                Light&Dark
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="banner__left">
+                <div className="banner__left__img">
+                   
+                </div>
+            </div>
+        </div>
+      </section>
     );
 }
 
