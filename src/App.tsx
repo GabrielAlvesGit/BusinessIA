7cee1e727d1bc83219c7fa84afce7a30a9afc8ac
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-08-18 17:17:08 -0300

    fix:removendo o footer no ./login pelas rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+// 2 - Reaproveitamento  de estrutura 
+import { Outlet, useLocation } from 'react-router-dom';
+
 import './styles/scss/App.scss';
 import Header from './components/global/Header';
 import Footer from './components/global/Footer';
 
-// 2 - Reaproveitamento  de estrutura 
-import { Outlet } from 'react-router-dom';
 
 const App: React.FC = () => {
+    const location = useLocation();
+
     return (
         <>
             <Header />
                 <Outlet />
-            <Footer />
+                {location.pathname !== '/login' && <Footer />}
         </>
     );
 };
